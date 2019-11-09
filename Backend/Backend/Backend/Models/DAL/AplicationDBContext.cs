5e9f2e222c8b53cff85daa7a0d4a9b5b2cb5a8ed
RyuDesire <51723745+RyuDesire@users.noreply.github.com>
2019-11-09 12:20:16 -0500

    Primer commit Tarjeta de credito

---
+++ Backend/Backend/Backend/Models/DAL/AplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Model.DAL
+{
+    public class AplicationDBContext:DbContext
+    {
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<DetallePago> DetallePago { get; set; }
+
+    }
+}
